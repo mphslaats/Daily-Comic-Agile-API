32c9e5764182bfd1e127a01a6c772667cc01915a
Glitch (glitch-hello-node) <none>
2022-01-31 13:58:53 +0000

    🍫👵 Checkpoint ./server.js:41723650/2080 ./package.json:41723650/102

--- server.js
+++ server.js
@@ -1,123 +1,61 @@
-/**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cheerio = require('cheerio');
+const nunjucks = require('nunjucks');
 
-const path = require("path");
+const today = new Date();
+const year = today.getFullYear();
+let month = today.getMonth() + 1
+month = month < 10 ? `0${month}`: month;
+let day = today.getDate();
+day = day < 10 ? `0${day}`: day;
+const date = `${year}-${month}-${day}`;
+const url = `https://www.comicagile.net/`
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
 
-
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
 
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
-  }
+app.get('/', (req, res) => {
+  axios.get(url).then(response => {
+    const $ = cheerio.load(response.data);
+    const comicTitle = $('#spliced-comic').find('h2').text();
+    const comicUrl = ('#spliced-comic').find('img').attr('src');
+    res.render('index.html', {
+      "title": comicTitle,
+      "image": `${comicUrl}.png`
+    });
+  }).catch(error => res.json(error));
 });
 
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
 
-/**
-* Our home page route
-*
-* Returns src/pages/index.hbs with data built into it
-*/
-fastify.get("/", function(request, reply) {
-  
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-  
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-    
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo
+app.get('/json', (req, res) => {
+  axios.get(url).then(response => {
+    const $ = cheerio.load(response.data);
+    const comicTitle = $('#spliced-comic').find('h2').text();
+    const comicUrl = ('#spliced-comic').find('img').attr('src');
+    const jsonResponse = {
+      "title": comicTitle,
+      "image": `${comicUrl}.png`,
     };
-  }
-  
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  reply.view("/src/pages/index.hbs", params);
+    res.json(jsonResponse);
+  }).catch(error => res.json(error));
 });
 
-/**
-* Our POST route to handle and react to form submissions 
-*
-* Accepts body data indicating the user choice
-*/
-fastify.post("/", function(request, reply) {
-  
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-  
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-  
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-    
-    // Load our color data file
-    const colors = require("./src/colors.json");
-    
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
-      };
-    } else {
-      
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
-  }
-  
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  reply.view("/src/pages/index.hbs", params);
-});
 
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
