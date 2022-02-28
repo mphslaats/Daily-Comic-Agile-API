42774ccbacf1e2fafe518cdf6a7874f027e636b9
Glitch (glitch-hello-node) <none>
2022-02-28 20:16:44 +0000

    ❣️🐕 Checkpoint ./views/index.html:41723650/461 ./server.js:41723650/1713

--- server.js
+++ server.js
@@ -63,6 +63,27 @@ app.get("/json", (req, res) => {
     .catch((error) => res.json(error));
 });
 
+app.get("/search", (req, res) => {
+  axios
+    .get(url)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const comic = $("#spliced-comic")
+      
+      const comicTitle = $("#spliced-comic").find("h2").text();
+      const comicUrl = $("#spliced-comic").find("img").attr("src");
+      const comicText = $("#spliced-comic").find("p").text();
+    
+      const jsonResponse = {
+        title: comicTitle,
+        image: comicUrl,
+        text: comicText,
+      };
+      res.json(jsonResponse);
+    })
+    .catch((error) => res.json(error));
+});
+
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
